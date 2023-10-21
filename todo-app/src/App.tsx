0ffdd0c99af458740185fcc7bacabd949e877a6e
Lakshay <lakshay.sbp@gmail.com>
2023-10-21 19:27:22 +0530

    need to build InputField Component

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './component/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <span className='heading'>Todo List</span>
+      <InputField/>
     </div>
   );
 }
