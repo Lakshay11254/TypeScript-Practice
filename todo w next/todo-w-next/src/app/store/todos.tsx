70076b8c2d1daf0dfdd0e162df2b00d83b87f2d1
Lakshay <lakshay.sbp@gmail.com>
2023-10-25 14:20:43 +0530

    new types created

--- todo w next/todo-w-next/src/app/store/todos.tsx
+++ todo w next/todo-w-next/src/app/store/todos.tsx
@@ -1,17 +1,47 @@
 //CONTEXT API
 
 import React, { ReactNode, createContext, useState } from 'react'
+export type Todo = {
+  id:string;
+  task:string;
+  completed:boolean;
+  createdAt:Date;  
+}
+export type TodosContext= {
+  todos:Todo[];
+
+  //call signature
+  handleAddTodo : (task:string) => void ;
+}
+
 
 export const todosContext = createContext(null) 
 
 export const TodosProvider = (children: {children:ReactNode}) => {
   
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState<Todo[]>([]);
   const handleAddTodo = (task:string) => {
 
-    setTodos(()=> {
-      
-    })
+    setTodos((prev)=> {
+      const newTodos: Todo[] = [{
+        id: Math.random().toString(),
+        task,
+      completed: false,
+      createdAt: new Date() 
+      },
+      ...prev
+    ]
+    
+    return newTodos;
+  }
+  
+    )
 
   }
+return 
+(
+  <todosContext.Provider value={{todos,handleAddTodo}}>
+    {children}
+  </todosContext.Provider>
+)
 }
