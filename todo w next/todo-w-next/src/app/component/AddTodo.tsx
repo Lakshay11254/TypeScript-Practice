15a94bf8c2a184bed5d9a84be6b576e9c93e45fb
Lakshay <lakshay.sbp@gmail.com>
2023-10-26 14:33:44 +0530

    navbar left

--- todo w next/todo-w-next/src/app/component/AddTodo.tsx
+++ todo w next/todo-w-next/src/app/component/AddTodo.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React, { FormEvent, useState } from "react";
+import { useTodos } from "../store/todos";
 
 const AddTodo = () => {
   const [todo, setTodo] = useState("");
+  const {handleAddTodo} = useTodos();
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     handleAddTodo(todo);
-    setTodo(value:"")
+    setTodo("")
   };
 
   return (
--- todo w next/todo-w-next/src/app/page.tsx
+++ todo w next/todo-w-next/src/app/page.tsx
@@ -1,13 +1,36 @@
+import "./globals.css";
 import React from 'react'
+// import { RiTodoLine } from "react-icons/ri";
 import AddTodo from './component/AddTodo'
+import {Todos} from './component/Todos'
+import Navbar from "./component/navbar";
+
 
 const page = () => {
   return (
     <main>
+       {/* <h2><RiTodoLine className="icons" /> TODO NEXT + TYPESCRIPT <RiTodoLine className="icons" /> </h2> */}
       <h2>TODO NEXT + TYPESCRIPT</h2>
+<Navbar />
       <AddTodo/>
+      <Todos/>
     </main>
   )
 }
 
 export default page
+
+// import AddTodo from "@/components/add-todo";
+// import {Todos} from "@/components/todos";
+// import Navbar from "@/components/navbar";
+// // thapa technical SUBSCRIBE
+// const Page = () => {
+//     return (
+//       <main>
+//           <AddTodo />
+//           <Todos />
+//       </main>
+//     );
+// };
+
+// export default Page;
